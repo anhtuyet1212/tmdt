8b232af6ec9d08e62ca493f9dcc10e8dbc7ce0e7
emladeyeu130799 <emladeyeu130799@gmail.com>
2022-04-08 11:43:36 +0700

    updayte

--- tmdt/Models/QLBH.Designer.cs
+++ tmdt/Models/QLBH.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\du lieu O C\LÊ HIỆP KIÊN 17S1100008 ĐS CNTT 12\tmdt\tmdt\Models\QLBH.edmx'. 
+﻿// T4 code generation is enabled for model 'D:\du lieu O C\anhtuyet\tmdt\tmdt\Models\QLBH.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
